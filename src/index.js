7574b6b3846adfaacfe702ab8776fe5aec6f2782
RileyManda <rileymanda69@gmail.com>
2023-06-13 21:49:19 +0200

    Implemented drag and drop

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import refreshIcon from './assets/refresh-icon.png';
 import backspaceIcon from './assets/back-space-icon.png';
 import moreIcon from './assets/more-vert.png';
 import './index.css';
-// array of todo items
 
 const TodoItems = [
   {
@@ -16,17 +15,20 @@ const TodoItems = [
     completed: false,
   },
 ];
-export default { TodoItems };
+
+let dragItem = null;
 
 const iterateTodoItems = () => {
   const todoList = document.getElementById('todo-list');
   TodoItems.forEach((todoItem) => {
     const listItem = document.createElement('li');
+    listItem.draggable = true;
+
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+    checkbox.classList.add('gray-checkbox');
 
     const label = document.createElement('label');
-    // label.textContent = `${todoItem.index}: ${todoItem.description}`;
     label.textContent = todoItem.description;
 
     const moreIconElement = document.createElement('img');
@@ -41,6 +43,42 @@ const iterateTodoItems = () => {
   });
 };
 
+const handleDragStart = (event) => {
+  dragItem = event.target;
+  event.dataTransfer.setData('text/plain', '');
+  dragItem.classList.add('dragging');
+};
+
+const handleDragOver = (event) => {
+  event.preventDefault();
+};
+
+const handleDragEnter = (event) => {
+  event.preventDefault();
+  event.target.classList.add('drag-over');
+};
+
+const handleDragLeave = (event) => {
+  event.target.classList.remove('drag-over');
+};
+
+const handleDrop = (event) => {
+  event.preventDefault();
+  const dropTarget = event.target;
+  const list = dropTarget.closest('.list-items');
+  const listItem = dragItem;
+  const targetIndex = Array.from(list.children).indexOf(dropTarget);
+
+  list.removeChild(listItem);
+  list.insertBefore(listItem, list.children[targetIndex]);
+  dropTarget.classList.remove('drag-over');
+};
+
+const handleDragEnd = () => {
+  dragItem.classList.remove('dragging');
+  dragItem = null;
+};
+
 iterateTodoItems();
 
 const header = document.querySelector('.card-header');
@@ -48,7 +86,6 @@ const refreshIconElement = document.createElement('img');
 refreshIconElement.src = refreshIcon;
 refreshIconElement.alt = 'Refresh icon';
 refreshIconElement.classList.add('refresh-icon');
-
 header.appendChild(refreshIconElement);
 
 const backspaceIconElement = document.createElement('img');
@@ -58,3 +95,11 @@ backspaceIconElement.classList.add('backspace-icon');
 
 const inputField = document.querySelector('.add-item');
 inputField.appendChild(backspaceIconElement);
+
+const todoList = document.getElementById('todo-list');
+todoList.addEventListener('dragstart', handleDragStart);
+todoList.addEventListener('dragover', handleDragOver);
+todoList.addEventListener('dragenter', handleDragEnter);
+todoList.addEventListener('dragleave', handleDragLeave);
+todoList.addEventListener('drop', handleDrop);
+todoList.addEventListener('dragend', handleDragEnd);
