4f3b4df52458c585afc8ded5327ab23b88af2686
RileyManda <rileymanda69@gmail.com>
2023-06-13 13:51:31 +0200

    Implemented todo list function itiration and html tags

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import _ from 'lodash';
+// array of todo items
 
-function component() {
-  const element = document.createElement('div');
+const TodoItems = [
+  {
+    index: 1,
+    description: 'Task1',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Task2',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Task3',
+    completed: false,
+  },
+];
+export default { TodoItems };
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const iterateTodoItems = () => {
+  const todoList = document.getElementById('todo-list');
 
-  return element;
-}
+  TodoItems.forEach((todoItem) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${todoItem.index}: ${todoItem.description}`;
+    todoList.appendChild(listItem);
+  });
+};
 
-document.body.appendChild(component());
+iterateTodoItems();
