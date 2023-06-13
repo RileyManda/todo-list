adb546c97963247b43c142c30e68042e86aa2e55
RileyManda <rileymanda69@gmail.com>
2023-06-14 00:39:43 +0200

    Bug fix click event

--- src/clickUtils.js
+++ src/clickUtils.js
@@ -1,20 +1,23 @@
 const handleClick = (event, inputField, moreIcon, dustbinIcon) => {
-  const listItem = event.currentTarget;
-  const label = listItem.querySelector('label');
-  const moreIconElement = listItem.querySelector('.more-icon');
+  const listItem = event.target.closest('li');
+  if (listItem) {
+    const label = listItem.querySelector('label');
+    const moreIconElement = listItem.querySelector('.more-icon');
 
-  if (listItem.classList.contains('editing')) {
-    // Save the changes
-    label.textContent = inputField.value;
-    listItem.classList.remove('editing');
-    moreIconElement.src = moreIcon;
-    moreIconElement.alt = 'More Icon';
-  } else {
-    // Enter edit mode
-    inputField.value = label.textContent;
-    listItem.classList.add('editing');
-    moreIconElement.src = dustbinIcon;
-    moreIconElement.alt = 'Dustbin Icon';
+    if (listItem.classList.contains('editing')) {
+      // Save the changes
+      listItem.classList.remove('editing');
+      label.textContent = inputField.value;
+      moreIconElement.src = moreIcon;
+      moreIconElement.alt = 'More Icon';
+    } else {
+      // Enter edit mode
+      listItem.classList.add('editing');
+      inputField.value = label.textContent;
+      inputField.focus();
+      moreIconElement.src = dustbinIcon;
+      moreIconElement.alt = 'Dustbin Icon';
+    }
   }
 };
 
